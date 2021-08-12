05f9dcb38ea9d0fccc458513fff6cafaead74006
hurmatullah <hrasaa1@gmail.com>
2021-08-12 09:52:29 +0430

    Fixed some errors in browser console

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,7 +31,9 @@
 
     <script src="{{ mix('/js/app.js') }}" defer></script>
     <script src="{{ asset('/js/jquery.min.js')}}"></script>
-    <script src="{{ asset('/js/typed.js')}}"></script>
+    {{-- <script src="{{ asset('/js/typed.js')}}"></script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.0/typed.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script defer src="{{ asset('js/popper.min.js')}}"></script>
     <script defer src="{{ asset('js/bootstrap.min.js')}}"></script>
     <script defer src="{{ asset('js/idangerous.swiper.min.js')}}"></script>
