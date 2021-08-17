f73ad041a336d49b208fa6cb78130595fb8f3af6
hurmatullah <hrasaa1@gmail.com>
2021-08-17 14:40:12 +0430

    Updated ssl FAQ  tab

--- resources/js/Pages/Services/SSLCertificate.vue
+++ resources/js/Pages/Services/SSLCertificate.vue
@@ -256,43 +256,6 @@
         </div>
       </div>
     </section>
-    <!-- ***** HELP ***** -->
-    <section class="help pt-4 pb-80">
-      <div class="container">
-        <div class="service-wrap">
-          <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-6">
-              <div class="help-container">
-                <div class="plans badge feat left bg-grey"><i class="fas fa-long-arrow-alt-left"></i></div>
-                <a href="hosting" class="help-item">
-                  <div class="img">
-                    <img class="svg ico" src="fonts/svg/cloudfiber.svg" alt="" />
-                  </div>
-                  <div class="inform">
-                    <div class="title">Go to Shared Hosting</div>
-                    <div class="description">Need similar power but a simpler solution?</div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-6">
-              <div class="help-container">
-                <div class="plans badge feat bg-grey"><i class="fas fa-long-arrow-alt-right"></i></div>
-                <a href="dedicated" class="help-item">
-                  <div class="img">
-                    <img class="svg ico" src="fonts/svg/dedicated.svg" alt="" />
-                  </div>
-                  <div class="inform">
-                    <div class="title">Go to Dedicated Server</div>
-                    <div class="description">Need more power and resources?</div>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
     <footers></footers>
   </div>
 </template>
